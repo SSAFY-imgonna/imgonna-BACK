efe56c857305787bb62aa9cdcbf31f2c100c0921
wus22 <seojihyeon99@naver.com>
2023-11-10 16:55:51 +0900

    [feat] qna 댓글 CRUD 기능 구현

---
+++ src/main/java/com/ssafy/trip/qna/comment/mapper/QnaCommentMapper.java
@@ -0,0 +1,24 @@
+package com.ssafy.trip.qna.comment.mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.trip.qna.comment.model.QnaComment;
+
+@Mapper
+public interface QnaCommentMapper {
+
+	void createQnaComment(QnaComment qnaComment) throws SQLException;
+
+	List<QnaComment> getQnaCommentList(Map<String, String> map) throws SQLException;
+
+	QnaComment getQnaComment(int commentNo) throws SQLException;
+
+	void modifyQnaComment(QnaComment qnaComment) throws SQLException;
+
+	void deleteQnaComment(int commentNo) throws SQLException;
+
+}
