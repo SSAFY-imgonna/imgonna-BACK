3f4063f607e632a3ee42454012058fcf3cf683f3
wus22 <seojihyeon99@naver.com>
2023-11-16 00:55:06 +0900

    [feat] 동행 댓글 crud 기능 구현

---
+++ src/main/java/com/ssafy/trip/accompany/comment/model/mapper/AccompanyCommentMapper.java
@@ -0,0 +1,23 @@
+package com.ssafy.trip.accompany.comment.model.mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.trip.accompany.comment.model.dto.AccompanyComment;
+
+@Mapper
+public interface AccompanyCommentMapper {
+
+	void createAccompanyComment(AccompanyComment accompanyComment) throws SQLException;
+
+	List<AccompanyComment> getAccompanyCommentList(Map<String, String> map) throws SQLException;
+
+	AccompanyComment getAccompanyComment(int commentNo) throws SQLException;
+
+	void modifyAccompanyComment(AccompanyComment accompanyComment) throws SQLException;
+
+	void deleteAccompanyComment(int commentNo) throws SQLException;	
+}
